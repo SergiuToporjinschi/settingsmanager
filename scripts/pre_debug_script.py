d1004c0cc5fcffd9bba944e9e994ed109d4346b3
sergiu_toporjinschi <sergiu.toporjinschi@gmail.com>
2019-10-20 15:01:35 +0300

    check main file exists before delete

--- scripts/pre_debug_script.py
+++ scripts/pre_debug_script.py
@@ -4,6 +4,7 @@ from distutils.dir_util import copy_tree
 import shutil, os
 
 copy_tree("src" , "test")
-os.remove("test/main.cpp")
+if os.path.exists("test/main.cpp"):
+    os.remove("test/main.cpp")
 
 print('### Finish Pre-debug script ########################')
